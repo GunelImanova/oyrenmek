744a7b1560424898373f3ac78fe4840a24213090
Gunel Imanova <imanovaaa07@gmail.com>
2024-03-27 13:42:12 +0400

    java js

---
+++ week1/week3/task4/java.js
@@ -0,0 +1,3 @@
+let num = Number (prompt("dogum ilinizi daxil edin"))
+let yas=2024-num
+console.log(`${yas} yas`)
---
+++ week1/week3/task6/java.js
@@ -0,0 +1,4 @@
+let num1 = Number(prompt(" a ededi daxil edin"))
+let num2 = Number(prompt(" b ededi daxil edin"))
+let sahe=parseInt (num1*num2)/2
+console.log(`${sahe} sahe`)
