388fbe8c7318f1aa502872ec2766673947a28132
Gunel Imanova <imanovaaa07@gmail.com>
2024-03-28 13:45:03 +0400

    day2

---
+++ week1/week3/day2/task11/java.js
@@ -0,0 +1,6 @@
+let eded = Number(prompt("ededi daxil et"))
+let count = 1
+for(let i=1; i<=eded; i++){
+    count*=i
+}
+console.log(count)
---
+++ week1/week3/day2/task2/java.js
@@ -0,0 +1,4 @@
+let num = Number(prompt("ededi daxil edin"))
+for (let i = num; i > 0; i--) {
+console.log(i)
+  }
