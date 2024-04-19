d3a0cb10cab0b60f20cb2b7b950202bb466cc7e6
Gunel Imanova <imanovaaa07@gmail.com>
2024-04-19 13:52:14 +0400

    up

--- week5/day5/23. JS (API requests, axios)/axios-advanced/assets/js/API/base_url.js
+++
@@ -1,3 +0,0 @@
-const API_URL = 'https://6620b3fe3bf790e070b059a1.mockapi.io/songs';
-
-export default API_URL;
---
+++ week5/day5/24. JS (json-server, recap)/movies-api-task/assets/js/add.js
@@ -0,0 +1,33 @@
+import { post } from "./API/requests/index.js";
+import { endpoints } from "./API/constants.js";
+
+const titleInp = document.querySelector("#title");
+const posterInp = document.querySelector("#poster");
+const trailerURLInp = document.querySelector("#trailerURL");
+const genreInp = document.querySelector("#genre");
+const ageInp = document.querySelector("#age");
+const countryInp = document.querySelector("#country");
+const directorInp = document.querySelector("#director");
+const descTextArea = document.querySelector("#desc");
+const backtohome=document.getElementById("backtohome")
+const addMoviebtn = document.getElementById("addMoviebtn")
+addMoviebtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    let newMovie = {
+
+        "title": titleInp.value,
+        "genre": genreInp.value,
+        "country": countryInp.value,
+        "director": directorInp.value,
+        "ageRestriction": ageInp.value,
+        "poster": posterInp.value,
+        "trailerURL": trailerURLInp.value,
+        "description": descTextArea.value,
+        "id": "7",
+    }
+    post(endpoints.movies, newMovie)
+})
+
+backtohome.addEventListener('click',(e)=>{
+window.location.replace("/movies-api-task/index.html")
+})
